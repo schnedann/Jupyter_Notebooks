d2ab75176fcc9e22eec10515df211347a60ffd61
schnedann <schnedann@users.noreply.github.com>
2021-01-16 15:25:20 +0100

    snapshot on floating Point issues and measure capacitor stuff

---
+++ octave/Electronic/measure_capacitor/doc/first_order_high_pass.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+
+fig=0
+fg=1e4;
+freq=linspace(0,1e6,2048);
+Hre=1./(1.+(fg./freq).^2);
+
+Him=(freq./fg)./(1.+(freq./fg).^2);
+
+F=atan(freq./fg);
+
+fig++;
+figure(fig)
+hold on
+#semilogx(freq,Hre);
+#semilogx(freq,Him);
+loglog(freq,Hre,"-db");
+loglog(freq,Him,":sr");
+hold off
+grid on;
+title("High Pass Filter");
+xlabel("Frequency");
+ylabel("H");
+legend("Real Part of H","Imaginary Part of H");
+
+fig++;
+figure(fig)
+loglog(freq,F);
+hold off
+grid on;
+title("High Pass Filter");
+xlabel("Frequency");
+ylabel("Phase");
---
+++ octave/Electronic/measure_capacitor/doc/first_order_low_pass.m
@@ -0,0 +1,47 @@
+clear all;
+close all;
+
+fig=0
+fg=1e4;
+freq=linspace(0,1e6,2048);
+Hre=1./(1.+(freq./fg).^2);
+
+Him=(-(freq./fg))./(1.+(freq./fg).^2);
+
+F=atan(-(freq./fg));
+
+fig++;
+figure(fig)
+hold on
+#semilogx(freq,Hre);
+#semilogx(freq,Him);
+loglog(freq,Hre,"-db");
+loglog(freq,Him,":sr");
+hold off
+grid on;
+title("Low Pass Filter");
+xlabel("Frequency");
+ylabel("H");
+legend("Real Part of H","Imaginary Part of H");
+
+fig++;
+figure(fig)
+hold on
+#loglog(freq,Hre,"-db");
+loglog(freq,Him,":sr");
+hold off
+grid on;
+title("Low Pass Filter");
+xlabel("Frequency");
+ylabel("H");
+#legend("Real Part of H","Imaginary Part of H");
+
+fig++;
+figure(fig)
+#loglog(freq,F);
+semilogx(freq,F);
+hold off
+grid on;
+title("Low Pass Filter");
+xlabel("Frequency");
+ylabel("Phase");
